5335d46f8c9616645782f88feed326aff6290bd6
DmytroShapoval <101992246+DimaShapoval@users.noreply.github.com>
2023-02-27 21:13:26 +0200

    Delete src directory

--- src/redux/store.js
+++
@@ -1,6 +0,0 @@
-import { applyMiddleware, createStore } from 'redux';
-import { composeWithDevTools } from 'redux-devtools-extension';
-import thunk from 'redux-thunk';
-import { rootReducer } from './redusers-combiner';
-
-export const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(thunk)));
